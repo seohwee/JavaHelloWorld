a4ff11db6e831366fd7963fc2d716414ea64124e
seohwee <22000263@handong.edu>
2021-09-04 23:52:12 +0900

    2nd commit

--- src/main/java/com/example/lab1/Main.java
+++ src/main/java/com/example/lab1/Main.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner s = new Scanner(System.in);
 		System.out.println("Hello World!!!\n");
 		System.out.print("이름 입력 : ");
